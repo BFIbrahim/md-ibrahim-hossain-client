42cb9119dfa992c953b33a5357e295ec4df869a8
Md Ibrahim <mdibrahim36194@gmail.com>
2023-06-24 10:35:34 -0700

    project setup done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=''>
+            <h1 className='text-5xl font-bold'>I LOVE YOU GOLOMOLO BOW
+                <span className='text-red-500 uppercase'> Rabeya Bosori RIYA</span></h1>
+
+            
+        </div>
+    );
+};
+
+export default Home;
