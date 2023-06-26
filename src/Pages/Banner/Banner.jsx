172a79d7ef4963f6f7a3d418be62497d6ad6dcc5
Md Ibrahim <mdibrahim36194@gmail.com>
2023-06-25 23:42:46 -0700

    download feature added

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,15 +1,63 @@
 import React from 'react';
 import './Banner.css'
+import Typewriter from 'typewriter-effect'
+import Lottie from 'lottie-react'
+import FacebookAnimation from '../../assets/Facebook logo animation.json'
+import LinkedInAnimation from '../../assets/LinkedIn logo animation.json'
+import GitHubAnimation from '../../assets/Github Animation.json'
+import TwitterAnimation from '../../assets/twitter logo animation.json'
+import { Link } from 'react-router-dom';
+
+
+const fileURL = 'Md._Ibrahim_developer_resume.pdf'
 
 const Banner = () => {
+
+    const hundleDownloadResume = (url) => {
+        const angcor = document.createElement('a')
+        angcor.href = url
+        angcor.setAttribute('download', "Md. Ibrahim developer resume.pdf")
+        document.body.appendChild(angcor)
+        angcor.click()
+        angcor.remove()
+    }
+
     return (
         <div className='banner-wrap md:h-96'>
             <div className='bg-[#000921] bg-opacity-80 md:h-96'>
-                <div className='w-96 mx-auto py-10 md:py-24 lg:py-24 text-center'>
-                    <img className='rounded-full w-40 h-40 mx-auto border-x-8 border-gray-400' src="https://i.postimg.cc/LXLfcW2x/Md-Ibrahim.jpg" alt="" />
-                    <h1 className='text-2xl font-bold text-center text-white'>Md. Ibrahim Hossain</h1>
-                    <p className='text-white text-center text-red-500 font-semibold'>Web Developer</p>
-                    <button className='btn border-none text-white bg-red-500 mt-5 hover:bg-gray-300 hover:bg-opacity-40'>Download Resume</button>
+                <div className='md:flex items-center py-10 md:py-20 lg:py-20 text-center md:text-start lg:text-start'>
+
+                    <div className='w-96'>
+                        <img className='rounded-full w-36 md:w-60 lg:w-60 h-36 md:h-60 lg:h-60 border-x-8 border-gray-400 mx-auto mt-5' src="https://i.postimg.cc/LXLfcW2x/Md-Ibrahim.jpg" alt="" />
+                    </div>
+
+                    <div className='md:w-[600px]'>
+                        <p className='text-red-500 font-semibold mt-2'>Hi There...</p>
+                        <h1 className='font-bold text-1xl  md:text-2xl lg:text-2xl text-white uppercase mt-2'>
+                            <Typewriter
+                                options={{
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 100,
+                                    strings: ['I am Md. Ibrahim Hossain', 'I am a web developer', 'I am a MERN stack developer']
+                                }}
+                            />
+                        </h1>
+
+                        <p className='text-white mt-5 px-3 md:px-0 lg:px-0'>Innovative MERN stack developer crafting dynamic web applications, merging creativity with expertise. Let's create exceptional digital experiences!</p>
+
+                       <button onClick={() => {hundleDownloadResume(fileURL)}} className='btn border-none text-white bg-red-500 mt-5 hover:bg-gray-300 hover:bg-opacity-40'>Download Resume</button>
+                    </div>
+
+                    <div className='flex md:flex-col lg:flex-col w-36 mx-auto mt-5'>
+                        <Link to='https://www.linkedin.com/in/md-ibrahim-hossain-6a7a5127b/' target="_blank"><Lottie className='w-8 hover:cursor-pointer' animationData={LinkedInAnimation} /></Link>
+
+                        <Link to='https://github.com/BFIbrahim' target="_blank"><Lottie className='w-10 hover:cursor-pointer' animationData={GitHubAnimation} /></Link>
+
+                        <Link to='https://www.facebook.com/profile.php?id=100093663434993' target="_blank"><Lottie className='w-10 hover:cursor-pointer' animationData={FacebookAnimation} /></Link>
+
+                        <Link to='https://twitter.com/MdIbrahim361' target="_blank"><Lottie className='w-10 hover:cursor-pointer' animationData={TwitterAnimation} /></Link>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Navigation/Navigation.jsx
+++ src/Pages/Navigation/Navigation.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const fileURL = 'Md._Ibrahim_developer_resume.pdf'
+
 
 const Navigation = () => {
+
+    const hundleDownloadResume = (url) => {
+        const angcor = document.createElement('a')
+        angcor.href = url
+        angcor.setAttribute('download', "Md. Ibrahim developer resume.pdf")
+        document.body.appendChild(angcor)
+        angcor.click()
+        angcor.remove()
+    }
+
     return (
         <div className="navbar fixed z-10  md:max-w-6xl mx-auto" >
             <div className="navbar-start">
@@ -9,36 +24,30 @@ const Navigation = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link className='hover:text-red-500 hover:font-semibold'>Home</Link></li>
+
+                        <li><Link className='hover:text-red-500 hover:font-semibold'>About</Link></li>
+
+                        <li><Link className='hover:text-red-500 hover:font-semibold'>Projects</Link></li>
+
+                        <li><Link className='hover:text-red-500 hover:font-semibold'>Blog</Link></li>
+
+                        <li><Link onClick={() => { hundleDownloadResume(fileURL) }} className='hover:text-red-500 hover:font-semibold'>Download Resume</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl text-white font-bold">BFGC</a>
+                <img className='ps-5 w-12' src="https://i.postimg.cc/g0NXBD9d/layers.png" alt="" />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li className='text-white'><Link className='hover:text-red-500 hover:font-semibold'>Home</Link></li>
+                    <li className='text-white'><Link className='hover:text-red-500 hover:font-semibold'>About</Link></li>
+                    <li className='text-white'><Link className='hover:text-red-500 hover:font-semibold'>Projects</Link></li>
+                    <li className='text-white'><Link className='hover:text-red-500 hover:font-semibold'>Blog</Link></li>
+                    <li className='text-white'><Link onClick={() => { hundleDownloadResume(fileURL) }} className='hover:text-red-500 hover:font-semibold'>Download Resume</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <button className='btn border-none text-white bg-red-500 mt-5 hover:bg-gray-300 hover:bg-opacity-40'>Contact Me</button>
             </div>
         </div>
     );
